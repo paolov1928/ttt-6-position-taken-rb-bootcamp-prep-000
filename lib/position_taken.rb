6721226db9fbccc1abee63fcb45f10dfc7822595
ventura.paolo <ventura.paolo@gmail.com>
2018-09-17 23:28:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
+  if board[index] == nil || board[index] == " " || board[index] == ""
   return false
   elsif board[index] != nil
     return true
   end
 end
-board[index] == nil ||
-#
